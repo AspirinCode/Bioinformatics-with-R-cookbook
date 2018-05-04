5b25933ce9c1af7604f89ed60f9635340a4de289
Chengshu21 <shuxcy@126.com>
2018-05-04 09:51:54 +0800

    Add files via upload

---
+++ Chapter-1-bio-start-with-R-master/R/practice-installing libraries.r
@@ -0,0 +1,10 @@
+
+options(repos=structure(c(CRAN="http://mirrors.tuna.tsinghua.edu.cn/CRAN/")))
+
+install.packages("dplyr")
+
+source("http://www.bioconductor.org/biocLite.R")
+
+biocLite("biomaRt")
+
+
